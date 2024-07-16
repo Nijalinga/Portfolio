c012de5ade6e38206653f9d0d67d05df1935ef0f
Nijalinga <156662938+Nijalinga@users.noreply.github.com>
2024-07-16 20:39:22 +0530

    Update Experience.jsx

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import styles from "./Experience.module.css";
 import skills from "../../data/skills.json";
-// import history from "../../data/history.json";
+import history from "../../data/history.json";
 import { getImageUrl } from "../../utils";
 
 export const Experience = () => {
