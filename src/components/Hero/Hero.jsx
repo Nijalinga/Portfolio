8e2209865ffa1fd2395d80341e117cac7c5efd41
Nijalinga <156662938+Nijalinga@users.noreply.github.com>
2024-07-16 20:26:56 +0530

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -16,7 +16,7 @@ export const Hero = () => {
         </a>
       </div>
       <img
-        src={getImageUrl("heroImage.jpg")}
+        src={getImageUrl("heroImage.png")}
         alt="Hero image of me"
         className={styles.heroImg}
       />
